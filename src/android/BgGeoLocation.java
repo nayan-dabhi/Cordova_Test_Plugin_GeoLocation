81eb358e4a57b557987022b0530e7dc1ffbe7eb6
Nayan <nayan.dabhi@sphererays.net>
2016-03-20 12:14:34 +0530

    import packages in activity

--- src/android/BgGeoLocation.java
+++ src/android/BgGeoLocation.java
@@ -5,10 +5,19 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
 public class BgGeoLocation extends CordovaPlugin {
 
     public static String userId;
     public static String postURL;
+    public static AlarmManager alarmManager;
+    public static PendingIntent pendingIntent;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -18,10 +27,14 @@ public class BgGeoLocation extends CordovaPlugin {
             userId = parameters.getString("userId");
             postURL = parameters.getString("postURL");
 
-            callbackContext.success(" User Id : " + userId + ", \n postURL : "+ postURL);
-            return true;
+            initialize(callbackContext);
         } else {
             return false;
         }
+    }
+
+    private void initialize(CallbackContext callbackContext) {
+        callbackContext.success(" User Id : " + userId + ", \n postURL : "+ postURL);
+        return true;
     }
 }
