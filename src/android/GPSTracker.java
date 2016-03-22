a4de1b6665c147f9e58bc4f129c34e5d8318f515
Nayan <nayan.dabhi@sphererays.net>
2016-03-22 17:48:32 +0530

    change in jva file

--- src/android/GPSTracker.java
+++ src/android/GPSTracker.java
@@ -57,11 +57,7 @@ public class GPSTracker extends Service implements LocationListener {
 			} else {
 				if (isGPSEnabled) {
 					if (location == null) {
-						locationManager.requestLocationUpdates(
-							LocationManager.GPS_PROVIDER,
-							MIN_TIME_BW_UPDATES,
-							MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-						Log.d("GPS Enabled", "GPS Enabled");
+						locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
 
 						if (locationManager != null) {
 							location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
@@ -75,11 +71,7 @@ public class GPSTracker extends Service implements LocationListener {
 						}
 					}
 				} else if (isNetworkEnabled) {
-					locationManager.requestLocationUpdates(
-						LocationManager.NETWORK_PROVIDER,
-						MIN_TIME_BW_UPDATES,
-						MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-					Log.d("Network", "Network");
+					locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
 
 					if (locationManager != null) {
 						location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
