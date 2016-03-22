b69e3d5a43b7647ed97c6a60321dac17b62de489
Nayan <nayan.dabhi@sphererays.net>
2016-03-22 16:17:53 +0530

    change in java file

--- src/android/GPSTracker.java
+++ src/android/GPSTracker.java
@@ -18,20 +18,16 @@ public class GPSTracker extends Service implements LocationListener {
 
 	private final Context mContext;
 	protected LocationManager locationManager = null;
+	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+	private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
 
 	boolean isGPSEnabled = false;
 	boolean isNetworkEnabled = false;
 	boolean canGetLocation = false;
 
-	Location location; // location
-	double latitude; // latitude
-	double longitude; // longitude
-
-	// The minimum distance to change Updates in meters
-	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
-
-	// The minimum time between updates in milliseconds
-	private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+	Location location;
+	double latitude;
+	double longitude;
 
 	public GPSTracker(Context context) {
 		this.mContext = context;
